02d0b7678c4c7ee3123a75c8f612e3da2023c3d4
Bogdan Lupu <bogdan.lupu@sumup.com>
2025-12-05 16:52:38 +0000

    Index for meilisearch

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   meilisearch: {
     config: {
       host: env('MEILISEARCH_HOST', 'https://ms-d1c67c9d137c-33016.fra.meilisearch.io'),
-      apiKey: env('MEILISEARCH_API_KEY', '5d41a228f43240f3925af646b1f441dafa9cca86c3aafcb6b1fb62659a6cff81'),
+      apiKey: env('MEILISEARCH_API_KEY', '994f6388aea877c4e74667c40d7a66a229fde9250d4f1002ed51663db9168fe0'),
       article: {
         indexName: 'Consumer-KnowledgeBase',
         entriesQuery: {
@@ -11,39 +11,6 @@ module.exports = ({ env }) => ({
         settings: {
           searchableAttributes: ['title', 'body'],
           filterableAttributes: ['category', 'contentType'],
-          sortableAttributes: ['createdAt', 'updatedAt'],
-        },
-      },
-      category: {
-        indexName: 'Consumer-KnowledgeBase',
-        entriesQuery: {
-          limit: 1000,
-        },
-        settings: {
-          searchableAttributes: ['name', 'description'],
-          filterableAttributes: ['name', 'contentType'],
-          sortableAttributes: ['createdAt', 'updatedAt'],
-        },
-      },
-      about: {
-        indexName: 'Consumer-KnowledgeBase',
-        entriesQuery: {
-          limit: 1000,
-        },
-        settings: {
-          searchableAttributes: ['title'],
-          filterableAttributes: ['contentType'],
-          sortableAttributes: ['createdAt', 'updatedAt'],
-        },
-      },
-      global: {
-        indexName: 'Consumer-KnowledgeBase',
-        entriesQuery: {
-          limit: 1000,
-        },
-        settings: {
-          searchableAttributes: ['siteName', 'siteDescription'],
-          filterableAttributes: ['contentType'],
           sortableAttributes: ['createdAt', 'updatedAt'],
         },
       },
